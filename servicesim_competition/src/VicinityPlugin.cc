826d1d5ea1b580ba4546d1f8c875a6770b4465c2
mikaelarguedas <devnull@localhost>
2018-02-19 17:44:10 -0800

    make Vicinity Plugin compile the lise of Actors in the world (no pose estimation or publication yet)

--- servicesim_competition/src/VicinityPlugin.cc
+++ servicesim_competition/src/VicinityPlugin.cc
@@ -1,14 +1,30 @@
+/*
+ * Copyright (C) 2018 Open Source Robotics Foundation, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
 #include "VicinityPlugin.hh"
 
-using namespace gazebo;
+using namespace::servicesim;
 
-GZ_REGISTER_MODEL_PLUGIN(VicinityPlugin);
+GZ_REGISTER_MODEL_PLUGIN(servicesim::VicinityPlugin);
 
 ////////////////////////
 // Constructor
-VicinityPlugin::VicinityPlugin():ModelPlugin()
+VicinityPlugin::VicinityPlugin()
 {
-
 }
 
 //Destructor
@@ -17,35 +33,64 @@ VicinityPlugin::~VicinityPlugin()
 
 }
 
-void VicinityPlugin::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
+void VicinityPlugin::Load(gazebo::physics::ModelPtr _parent, sdf::ElementPtr _sdf)
 {
-  // store the pointer to the model_name
-  this->model = _parent;
-  this->world = _parent->GetWorld(); // Store the pointer to the world
+  this->threshold_ = 5.0;
 
-  std::string model_name = this->model->GetName();
-  std::string topic_name = "vicinity";
-  pub = nh.advertise<std_msgs::Bool>(topic_name,1);
-  paramName = "guest_name";
-  if (_sdf->HasElement(paramName))
+  this->model_ = _parent;
+  this->world_ = _parent->GetWorld(); // Store the pointer to the world
+
+  for (unsigned int i = 0; i < this->world_->ModelCount(); ++i)
   {
-    this->entity = world->GetEntity(_sdf->Get<std::string>(paramName));
+    auto model = this->world_->ModelByIndex(i);
+    if (gazebo::physics::ActorPtr a = boost::dynamic_pointer_cast<gazebo::physics::Actor>(model))
+    {
+      this->actorPtrs_.push_back(a);
+    }
   }
+  gzmsg << "Found " << this->actorPtrs_.size() << " actors in the world" << std::endl;
+
+  // std::string model_name = this->model->GetName();
+  // std::string topic_name = "vicinity";
+  // pub = nh.advertise<std_msgs::Bool>(topic_name,1);
+  // paramName = "guest_name";
+  // if (_sdf->HasElement(paramName))
+  // {
+  //   this->entity = world->GetEntity(_sdf->Get<std::string>(paramName));
+  // }
   paramName = "threshold";
   if (_sdf->HasElement(paramName))
   {
-    this->threshold = _sdf->Get<double>(paramName);
+    this->threshold_ = _sdf->Get<double>(paramName);
+  }
+  else
+  {
+    this->threshold_ = 5.0;
   }
 
-  //Listen to the update event
-  this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+  if (!_sdf->HasElement("updateRate"))
+  {
+    this->update_rate_ = 1.0;
+  }
+  else
+    this->update_rate_ = _sdf->GetElement("updateRate")->Get<double>();
+
+
+  this->last_time_ = this->world_->SimTime();
+  // Listen to the update event
+  this->updateConnection = gazebo::event::Events::ConnectWorldUpdateBegin(
     boost::bind(&VicinityPlugin::Update, this));
 }
 
 void VicinityPlugin::Update()
 {
-  auto robot_pos = this->model->WorldPose().Pos();
-  auto entity_pos = this->entity->WordlPose().Pos();
-
-
+  gazebo::common::Time cur_time = this->world_->SimTime();
+  // rate control
+  if (this->update_rate_ > 0 &&
+      (cur_time - this->last_time_).Double() < (1.0 / this->update_rate_))
+    return;
+  gzmsg << "Updating Vicinity" << std::endl;
+  this->last_time_ = cur_time;
+  // auto robot_pos = this->model->WorldPose().Pos();
+  // auto entity_pos = this->entity->WordlPose().Pos();
 }
--- servicesim_competition/src/VicinityPlugin.hh
+++ servicesim_competition/src/VicinityPlugin.hh
@@ -1,40 +1,63 @@
+/*
+ * Copyright (C) 2018 Open Source Robotics Foundation, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
 #ifndef GAZEBO_PLUGINS_VICINITY_PLUGIN_HH
 #define GAZEBO_PLUGINS_VICINITY_PLUGIN_HH
 
+#include <string>
+#include <vector>
+
 #include <boost/bind.hpp>
-#include <gazebo/gazebo.h>
+// #include <gazebo/gazebo.h>
 #include <gazebo/physics/physics.hh>
-#include <gazebo/common/common.hh>
+#include <gazebo/common/Plugin.hh>
 #include "ros/ros.h"
 #include <std_msgs/Bool.h>
 
-namespace gazebo
+namespace servicesim
 {
-  class VicinityPlugin: public ModelPlugin
+  class VicinityPlugin: public gazebo::ModelPlugin
   {
 
   public: VicinityPlugin();
   public: virtual ~VicinityPlugin();
-  public: void VicinityPlugin::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf);
+  public: void Load(gazebo::physics::ModelPtr _parent, sdf::ElementPtr _sdf);
   public: void Update();
 
   //Store pointer to the model
-  private: physics::ModelPtr model;
+  private: gazebo::physics::ModelPtr model_;
   //Store pointer to the world
-  private: physics::WorldPtr world;
+  private: gazebo::physics::WorldPtr world_;
   //Store pointer to the entity
-  private: physics::EntityPtr entity;
-  private: event::ConnectionPtr updateConnection;
+  private: gazebo::physics::EntityPtr entity_;
+  private: gazebo::event::ConnectionPtr updateConnection;
   private: std::string paramName;
-  private: std::double threshold;
+  private: double threshold_;
+  private: double distance_;
+  private: double update_rate_;
+  private: gazebo::common::Time last_time_;
 
-  private: std::double distance;
+  private: std::vector<gazebo::physics::ActorPtr> actorPtrs_;
   // private: ignition:math:Pose3d robot_pos, entity_pos;
   /// ROS stuff
   private: ros::NodeHandle nh;
   private: ros::Publisher pub;
   private: std_msgs::Bool flag;
-
-
-  }
+  };
 }
+
+#endif
