576a8e28de988d9e92653a55dc9234110d6b7927
mikaelarguedas <devnull@localhost>
2018-02-28 18:18:15 -0800

    add comment to the image processing example

--- competitor_example_solution/servicesim_example_python_solution/src/image_processing_example.py
+++ competitor_example_solution/servicesim_example_python_solution/src/image_processing_example.py
@@ -27,11 +27,16 @@ from sensor_msgs.msg import Image
 
 class ColorSegmentator(object):
     def __init__(self):
+        # create an instance of CVBridge, this will allow to convert ROS messages
+        #  to OpenCV CVMat and vice versa
         self.bridge = CvBridge()
-        self.roi_pub = rospy.Publisher(
+        # create a publisher to publish the modified image to
+        self.segmented_img_pub = rospy.Publisher(
             '/servicebot/camera_front/segmented_image', Image, queue_size=1)
+        # create a subscriber to receive the robots front camera image
         self.image_sub = rospy.Subscriber(
             '/servicebot/camera_front/image_raw', Image, self.image_callback)
+        # define a range of color to detect guest shirts
         self.boundaries = [
             # keep only dark red pixels
             # ([50, 17, 10], [200, 50, 56]),
@@ -42,7 +47,9 @@ class ColorSegmentator(object):
         ]
 
     def image_callback(self, msg):
+        # Function called each time an image is received from the robot
         try:
+            # convert the image to a CVMat
             cv_image = self.bridge.imgmsg_to_cv2(msg, 'rgb8')
         except CvBridgeError as e:
             rospy.logerr(e)
@@ -52,16 +59,18 @@ class ColorSegmentator(object):
             lower = numpy.array(lower, dtype='uint8')
             upper = numpy.array(upper, dtype='uint8')
 
-            # find the colors within the specified boundaries and apply
-            # the mask
+            # find the colors within the specified boundaries and create a mask out of it
             mask = cv2.inRange(cv_image, lower, upper)
+            # apply the mask to the image
             output = cv2.bitwise_and(cv_image, cv_image, mask=mask)
         try:
+            # convert the resulting image to a ros Image message
             masked_image_msg = self.bridge.cv2_to_imgmsg(output, 'rgb8')
         except CvBridgeError as e:
             rospy.logerr(e)
             return
-        self.roi_pub.publish(masked_image_msg)
+        # publish the resulting image
+        self.segmented_img_pub.publish(masked_image_msg)
 
 
 def blob_detector():
