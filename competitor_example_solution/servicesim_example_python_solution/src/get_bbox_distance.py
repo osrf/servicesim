d431607eb0a671f07ff6e4c17b1aed3671c426e8
rohitsalem <rohitsalem@gmail.com>
2018-03-20 15:51:12 -0700

    code clean up and some fixes

--- competitor_example_solution/servicesim_example_python_solution/src/get_bbox_distance.py
+++ competitor_example_solution/servicesim_example_python_solution/src/get_bbox_distance.py
@@ -19,18 +19,23 @@ import cv2
 from cv_bridge import CvBridge, CvBridgeError
 
 import numpy
-import imutils
+
 import rospy
+
 from servicesim_example_python_solution.msg import Contour
+
 from sensor_msgs.msg import Image
+
 from std_msgs.msg import Float64
+
+
 class BboxDetector(object):
     def __init__(self):
         # create an instance of CVBridge, this will allow to convert ROS messages
         #  to OpenCV CVMat and vice versa
         self.bridge = CvBridge()
         # create a publisher to publish the modified image to
-        self.segmented_img_pub = rospy.Publisher(
+        self.bbox_img_pub = rospy.Publisher(
             '/servicebot/camera_front/guest_bbox_detection', Image, queue_size=1)
         # create a publisher to publish the height, width and area of the contourArea
         self.pub = rospy.Publisher(
@@ -38,27 +43,11 @@ class BboxDetector(object):
         # create a subscriber to receive the robots front camera image
         self.image_sub = rospy.Subscriber(
             '/servicebot/camera_front/image_raw', Image, self.image_callback)
-        # define a range of color to detect guest shirts
-        self.boundaries = [
-            # keep only dark red pixels
-            # ([50, 17, 10], [200, 50, 56]),
-            # keep only green pixels
-            # ([10, 40, 5], [200, 50, 30]),
-            # keep only blue pixels
-            # ([5, 5, 50], [25, 25, 145])
-            #head color
-            # ([155,105, 75], [165, 115, 85])
-            #pant color- red
-            # ([95,95, 95], [110, 110, 110])
-            ([90, 0, 0], [115, 10, 10])
 
-
-        ]
-
+    # Returns the bounding box coordinates of the detected contour area,
+    #  given the threshold of the color of contour to be detected
     def get_bbox_coordinates(self, boundaries, cv_image):
-        self.boundaries = boundaries
-
-        for (lower, upper) in self.boundaries:
+        for (lower, upper) in boundaries:
             # create NumPy arrays from the boundaries
             lower = numpy.array(lower, dtype='uint8')
             upper = numpy.array(upper, dtype='uint8')
@@ -67,28 +56,32 @@ class BboxDetector(object):
             mask = cv2.inRange(cv_image, lower, upper)
             # apply the mask to the image
             output = cv2.bitwise_and(cv_image, cv_image, mask=mask)
-
+        # creating a gray scale image of the masked output
         output_gray = cv2.cvtColor(output, cv2.COLOR_RGB2GRAY)
         kernel = numpy.ones((15,15), numpy.uint8)
+        # Creating a binary image with some threshold and then dilating and eroding \
+        # the resulted image for a cleaner contour detection
         thresh = cv2.threshold(output_gray, 5, 255, cv2.THRESH_BINARY)[1]
         thresh = cv2.erode(thresh, None, iterations = 2)
         thresh = cv2.dilate(thresh, kernel, iterations = 2)
+        # Detecting the contours
         cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts  = cnts[0] if imutils.is_cv2() else cnts[1]
+        cnts  = cnts[1]
         if len(cnts)!=0:
+            # Detecting the contour with maximum area
             c = max(cnts, key = cv2.contourArea)
             area = cv2.contourArea(c)
-
+            # Getting the extremities of the max contour
             extLeft = tuple(c[c[:, :, 0].argmin()][0])
             extRight = tuple(c[c[:, :, 0].argmax()][0])
             extTop = tuple(c[c[:, :, 1].argmin()][0])
             extBot = tuple(c[c[:, :, 1].argmax()][0])
-
+            # Translating the extreme points of the max contour to bounding box coordinates \
+            # which encloses the max contour
             x_min = min(extLeft[0], extRight[0], extTop[0], extBot[0])
             x_max = max(extLeft[0], extRight[0], extTop[0], extBot[0])
             y_min = min(extLeft[1], extRight[1], extTop[1], extBot[1])
             y_max = max(extLeft[1], extRight[1], extTop[1], extBot[1])
-
             return [x_min, x_max, y_min, y_max]
 
 
@@ -101,20 +94,27 @@ class BboxDetector(object):
             rospy.logerr(e)
             return
 
+        # define a range of color to detect guest's shirt, here it is Blue
         boundaries_shirt = [
             # keep only blue pixels
-            # ([15, 15, 50], [25, 25, 80])]
-            ([5, 5, 50], [25, 25, 145])   ]
-        # define a range of color to detect guest shirts
+            ([5, 5, 50], [25, 25, 145])]
+        # define a range of color to detect guest's pants, here it is Red
         boundaries_pants = [
-            #pants color -red
+            # keep only red pixels
             ([90, 0, 0], [115, 10, 10])]
-            # ([95,95, 95], [110, 110, 110]) ]
 
+        # Getting the bounding box coordinates of the shirt and pants of the guest
         coordinates_pants = self.get_bbox_coordinates(boundaries_pants,cv_image)
         coordinates_shirts = self.get_bbox_coordinates(boundaries_shirt,cv_image)
         if (coordinates_pants and coordinates_shirts):
-            if (coordinates_pants[3]>coordinates_shirts[3] and coordinates_shirts[2]<coordinates_pants[2]):
+            # checking if the detected contour for the shirt \
+            #  is above pants along the same vertical height
+            if (coordinates_pants[3]>coordinates_shirts[3] and \
+                coordinates_shirts[2]<coordinates_pants[2] and \
+                coordinates_shirts[0]<coordinates_pants[1] and \
+                coordinates_pants[0]<coordinates_shirts[1]):
+
+                # Calculating the bounding box coordinates enclosing the whole of guest's body.
                 x_min = min(coordinates_shirts[0], coordinates_pants[0])
                 x_max = max(coordinates_shirts[1], coordinates_pants[1])
                 y_min = min(coordinates_shirts[2], coordinates_pants[2])
@@ -132,11 +132,9 @@ class BboxDetector(object):
                 # getting the height and width of the bounding box enclosing the contour
                 width = x_max - x_min
                 height = y_max - y_min
-                # print(x_min, x_max, y_min, y_max)
-                # print(width, height)
 
-                ### calculating the distance
-                # height of the person
+                ### Calculating the distance of the person from the robot's camera
+                # height of the person/guest
                 H = 1.5
                 # focal length of the camera
                 F = 550
@@ -145,6 +143,8 @@ class BboxDetector(object):
                 # distance of the person from the camera
                 D = (F*H)/P
                 contour_msg = Contour()
+
+                # populating the contour msg with guest's distance and bbox coordinates
                 contour_msg.distance = D
                 contour_msg.width = width
                 contour_msg.height = height
@@ -164,7 +164,7 @@ class BboxDetector(object):
             rospy.logerr(e)
             return
         # publish the resulting image
-        self.segmented_img_pub.publish(image_with_msg)
+        self.bbox_img_pub.publish(image_with_msg)
 
 
 def get_bbox_distance():
